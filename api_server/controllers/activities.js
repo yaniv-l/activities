f9971f32080a2e47a752107d9d02cf5fa84e7f29
yaniv-l <yanivl@srl.co.il>
2017-04-21 18:50:08 +0300

    activities.js update

--- api_server/controllers/activities.js
+++ api_server/controllers/activities.js
@@ -1,13 +1,17 @@
-const Activity = require('../modules/activity');
+const Activity = require('../models/activity');
 
 exports.postNewActivity = function(req, res, next) {
   // if user does not exists, then craete a new record
-  const activity = new Activity(req.body.activity);
+  const activity = new Activity(req.body);
+  console.log("Activity body is:");
+  console.log(req.body);
+
 
   activity.save(function(err) {
     // if fails to insert into db
     if(err) { return next(err); }
 
     // send back a success indications
-    res.json({ success: true, token: tokenForUser(user) });
+    res.json({ success: true });
+  });
 }
